02921fa54ea86fd62cef2d8f547347a954760d58
ArunS <Arun.S@unisys.com>
2017-01-04 11:30:25 +0530

    deleted old files

--- test/spec/controllers/controllers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('Controller: ngdemoApp.controllers', function () {
-
-  /* karma test runner fails if it can't find a test... */
-  it('should always be true', function () {
-    expect(true).toBe(true);
-  });
-});
